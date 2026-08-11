fe995887912da07cc65fe24c38a7125a7ea283bd
Seko <seifosamaaliii@gmail.com>
2026-08-11 03:05:44 +0300

    Added A Logger And Error Handler For The bronze_connect.py

--- Scripts/Bronze/bronze_ddl.sql
+++ Scripts/Bronze/bronze_ddl.sql
@@ -1,4 +1,17 @@
-CREATE TABLE bronze_stocks_price(
+/*
+============================================
+DDL Scripts: Create Bronze Tables
+============================================
+
+Script Purpose:
+	This Script Creates Tables In The 'Bronze' Schema, Dropping The Table If It's Already Exists.
+	Run This Script To Re-Define The DDL Strucutre Of The 'Bronze' Table.
+*/
+
+IF OBJECT_ID ('Bronze.stocks_prices', 'U') IS NOT NULL
+	DROP TABLE Bronze.stocks_prices
+
+CREATE TABLE Bronze.stocks_prices(
 	ticker			VARCHAR(12),
 	date			VARCHAR(12),
 	open_price		VARCHAR(12),
