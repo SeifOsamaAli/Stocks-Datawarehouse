8cdf62de57bc90478f8e649de2c433d87d5cc6de
Seko <seifosamaaliii@gmail.com>
2026-08-11 06:51:56 +0300

    I Just Changed The Named Of The Created Log When Inserting  The Data Into The Bronze Layer In Database

--- bronze_connect.py
+++ bronze_connect.py
@@ -34,7 +34,7 @@ from datetime import datetime
 now = datetime.now()
 timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler(f'Logs/bronze_inserting_data_{timestamp}.log')
+file_handler = logging.FileHandler(f'Logs/bronze_inserting_{timestamp}.log')
 
 logger.addHandler(file_handler)
 formatter = logging.Formatter(
